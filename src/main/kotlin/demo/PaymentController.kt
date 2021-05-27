a619298a5137b03a940b919d2b3cb21985b4558a
Jeff Fang <jeff.fang@airwallex.com>
2021-05-27 11:21:07 +0800

    test

--- src/main/kotlin/demo/PaymentController.kt
+++ src/main/kotlin/demo/PaymentController.kt
@@ -3,18 +3,16 @@ package demo
 import com.github.victools.jsonschema.generator.*
 import com.github.victools.jsonschema.module.javax.validation.JavaxValidationModule
 import com.github.victools.jsonschema.module.javax.validation.JavaxValidationOption
+import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.ModelAndView
 
 @Controller
 class PaymentController {
 
-    @GetMapping("/form/{country}")
-    fun paymentForm(@PathVariable country: String): ModelAndView {
-        println(country)
-
+    @GetMapping("/form/{type}")
+    fun paymentForm(@PathVariable type: String): ModelAndView {
         val module = JavaxValidationModule(
             JavaxValidationOption.INCLUDE_PATTERN_EXPRESSIONS,
             JavaxValidationOption.NOT_NULLABLE_FIELD_IS_REQUIRED
@@ -24,7 +22,37 @@ class PaymentController {
                 .with(module)
                 .build()
         val generator = SchemaGenerator(config)
-        val schema = generator.generateSchema(PaymentRequestDto::class.java).toString()
+        val schema = generator.generateSchema(
+            if (type == "swift") SwiftBankAccount::class.java else LocalBankAccount::class.java
+        ).toString()
         return ModelAndView("test", mapOf("schema" to schema))
     }
+
+    @GetMapping("/bankaccount/{type}", produces = ["application/json"])
+    @ResponseBody
+    fun bankAccount(@PathVariable type: String): BankAccount {
+        return when (type.toLowerCase()) {
+            "swift" -> SwiftBankAccount()
+            "local" -> LocalBankAccount()
+            else -> TODO()
+        }
+    }
+}
+
+@Schema(
+    type = "object",
+    title = "BankAccount",
+    subTypes = [SwiftBankAccount::class, LocalBankAccount::class]
+)
+abstract class BankAccount {
+    val accountNumber: String = ""
+}
+
+class SwiftBankAccount : BankAccount() {
+    val swiftCode: String = ""
+}
+
+
+class LocalBankAccount : BankAccount() {
+    val bankCode: String = ""
 }
